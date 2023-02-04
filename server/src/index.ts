6cec1faf96d53b9f9eb6c0085b0496498bc92943
Fortan Pireva <fortanpireva99@gmail.com>
2023-02-04 10:09:40 +0100

    final commit before branching

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import getUserRoutes from "./routes/userRoutes";
+import getVideoRoutes from "./routes/videoRoutes";
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(getUserRoutes);
+app.use(getVideoRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + Typescript Server");
